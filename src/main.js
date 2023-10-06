52df4ea404ef6cb3ac051e6680c60d66661bf68a
FeryPermana <muhammad.permana@students.amikom.ac.id>
2023-10-06 17:35:22 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//import router
+import router from './router'
+
+//import store vuex
+import store from './store'
+
+// //import store vuex
+// import store from './api/Api'
+
+const app = createApp(App)
+
+//gunakan router di vue js dengan plugin "use"
+app.use(router)
+
+//gunakan store di vue js dengan plugin "use"
+app.use(store)
+
+//define mixins for global function
+app.mixin({
+
+    methods: {
+
+        //money thousands
+        moneyFormat(number) {
+            let val = (number / 1).toFixed(2).replace('.', ',')
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+
+        },
+
+        //calculate discount
+        calculateDiscount(product) {
+            return product.price - (product.price * (product.discount) / 100)
+        }
+
+    }
+})
+
+app.mount('#app')
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="login">
+        <h2>LOGIN</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginComponent'
+}
+</script>
